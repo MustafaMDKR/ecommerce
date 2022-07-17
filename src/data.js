fb2df49d4dd8f0fc583d0a1e9f8b126b7771dd1c
Mustafa Madkour <mustafamdkr@gmail.com>
2022-07-17 17:27:40 +0200

    Intializing client side

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://www.seekpng.com/png/full/858-8584137_fashion-forward-episodes-offers-fashion.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS...",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://freepngimg.com/thumb/girl/31243-5-fashion-girl-clipart.png",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS...",
+        bg: "fcfled",
+    },
+    {
+        id: 3,
+        img: "https://cdn.pixabay.com/photo/2019/08/13/10/57/dancing-4403138_1280.png",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS...",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://www.carbon2cobalt.com/site/Products/133681_shirt-1.jpg?resizeid=3&resizeh=475&resizew=400",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/upwk61733181-wikimedia-image.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=2e858c1ef762041a8eb6ac74e0cd320e",
+        title: "LOUNGWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8vnnLJZCy1e-PZROsDNY06iWmE4LyaX1fSQlWmC7H3kPla4pGUeAG2T9eIRKarg0N-VM&usqp=CAU",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQUovOvLarCnI_AT3dCSu0ONBHm4rCoEIagrkfzCnNg5PVMYdXfHNv-9EVgtZaMOApaTDIrt6AcWeM-&usqp=CAc"
+    },
+    {
+        id: 2,
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRd2u1H88HFMBbhcKjQGNtKCmGiuB5CWOJdFJ0kJTcY8FZvbttCYlAfShXII5diME_uh5_4wBBpBrfj&usqp=CAc"
+    },
+    {
+        id: 3,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTLob2VADTyS1ZsW8N2ya92Ddyl_qLaz_ckNojy2isFWzeHGhMqmXm15eGkRZWdTFsmSQyNbYanXJtT&usqp=CAc"
+    },
+    {
+        id: 4,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRjW22hwzGB3fGuNUNh2n8KGXKwAP5Q78FakSvrWb36MOEKCJLft8MnDlF0ttHFwKbzZZrKqkxOhG0&usqp=CAc"
+    },
+    {
+        id: 5,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTLob2VADTyS1ZsW8N2ya92Ddyl_qLaz_ckNojy2isFWzeHGhMqmXm15eGkRZWdTFsmSQyNbYanXJtT&usqp=CAc"
+    },
+    {
+        id: 6,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSMin4LhereA6K2Wg4j8Boru_sjqNgXzw8EHJCxeZ-c3iU-xwsLCXwOFB4dh_rA36jcRR50-CCcniOE&usqp=CAc"
+    },
+    {
+        id: 7,
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTiUEHERq4lB7iHH2J6DdVH-jt1vQFniCr6y7HcwLuy0g-KF3Q6snf3-hJukE13QOVXSDGkOSDDoX8&usqp=CAc"
+    },
+    {
+        id: 8,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRwN__8niSFUVPtplQHG-bME9YHPs4CAYoWkVdEeGvk3Qf-HgZ0sJ58vlYu9XQAFH4qsIXflcbdo1c&usqp=CAc"
+    },
+    {
+        id: 9,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRaeY5YRGIURWA8xRSyalvErLDPKfmIhlhFOSwHcJUkGHJqC5dcigyybVS1XFuIIwnpqVPYyeFObs0&usqp=CAc"
+    },
+    {
+        id: 10,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTLob2VADTyS1ZsW8N2ya92Ddyl_qLaz_ckNojy2isFWzeHGhMqmXm15eGkRZWdTFsmSQyNbYanXJtT&usqp=CAc"
+    },
+];
