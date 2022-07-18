efea68a039147fcebcb9ead5e2bd3d88dfc2ba95
Mustafa Madkour <mustafamdkr@gmail.com>
2022-07-18 18:25:29 +0200

    Login & Register pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: url("https://images.pexels.com/photos/12025241/pexels-photo-12025241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    margin-bottom: 10px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Link = styled.a`
+    margin: 5px 0;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>Sign in</Title>
+            <Form>
+                <Input placeholder="UserName"/>
+                <Input placeholder="Password"/>
+                <Button>Sign in</Button>
+                <Link>Forgot your password?</Link>
+                <Link>Create new account</Link>
+            </Form>
+        </Wrapper>  
+    </Container>
+  )
+}
+
+export default Login
---
+++ src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: url("https://images.pexels.com/photos/12025241/pexels-photo-12025241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
+
+const Agreement = styled.span`
+    font-size: 12px;
+    padding: 20px 0px;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>Create New Account</Title>
+            <Form>
+                <Input placeholder="First Name"/>
+                <Input placeholder="Last Name"/>
+                <Input placeholder="UserName"/>
+                <Input placeholder="Email"/>
+                <Input placeholder="Password"/>
+                <Input placeholder="Confirm Password"/>
+                <Agreement>
+                    By creating an acoount, I consent to the processing of my personal
+                    data in accordance with the <b>PRIVACY POLICY</b>
+                </Agreement>
+                <Button>Register</Button>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Register
