4c5d2c65bc75b1c7c6cfcade15119755d40c65d1
Mustafa Madkour <mustafamdkr@gmail.com>
2022-07-19 06:44:38 +0200

    Cart page & Responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
-import Product from "./pages/Product"
+// import Product from "./pages/Product"
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Product />;
+  return <Cart />;
 };
 
 export default App;
